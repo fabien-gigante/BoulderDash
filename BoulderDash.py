96e7d837d78ef1279eb22518f4aee18c6b8ca152
fabien-gigante <fabien.gigante@free.fr>
2022-08-04 21:49:54 +0200

    Ajoutez des fichiers projet.

---
+++ BoulderDash.py
@@ -0,0 +1,37 @@
+import arcade
+from Miner import *
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Boulder Dash"
+
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.keys = []
+        self.sprites = None
+        self.camera = None
+
+    def setup(self):
+        self.sprites = arcade.SpriteList()
+        self.sprites.append( Miner(self, 196, 270) )
+
+        arcade.set_background_color(arcade.color.AZURE);
+        self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    def on_draw(self):
+        self.camera.use()
+        self.clear()
+        self.sprites.draw()
+
+    def on_key_press(self, key, modifiers): self.keys.append(key)
+    def on_key_release(self, key, modifiers): self.keys.remove(key)
+    def on_update(self, delta_time):
+        for sprite in self.sprites: sprite.on_update(delta_time)
+
+def main():
+    Game().setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
---
+++ Miner.py
@@ -0,0 +1,17 @@
+import arcade
+from BoulderDash import Game
+
+TILE_SCALING = 0.5
+MOVEMENT_SPEED = 5
+
+class Miner(arcade.Sprite):
+    def __init__(self, game: Game, x, y):
+        super().__init__(":resources:images/enemies/bee.png", TILE_SCALING)
+        self.game = game
+        self.center_x = x ; self.center_y = y
+
+    def on_update(self, delta_time):
+        if arcade.key.LEFT in self.game.keys:
+          self.center_x -= MOVEMENT_SPEED
+        elif arcade.key.RIGHT in self.game.keys:
+          self.center_x += MOVEMENT_SPEED
