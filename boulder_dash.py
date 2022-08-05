cc99a64a899b4879ccb8ebc0c59d6a6246a32a79
fabien-gigante <fabien.gigante@free.fr>
2022-08-05 10:57:56 +0200

    typing hints

--- boulder_dash.py
+++ boulder_dash.py
@@ -1,5 +1,6 @@
 import arcade
 import random
+from typing import Optional
 from elements import *
 
 STAGE_WIDTH = 24
@@ -9,9 +10,9 @@ SCREEN_HEIGHT = 48 * STAGE_HEIGHT
 SCREEN_TITLE = "Boulder Dash"
 
 class Stage:
-    def __init__(self, game, level):
+    def __init__(self, game: "Game", level: int) -> None:
         self.game = game
-        self.tiles = []
+        self.level = level ; self.tiles = []
         for i in range(0,STAGE_HEIGHT):
             self.tiles.append([])
             for j in range(0,STAGE_WIDTH):
@@ -23,31 +24,36 @@ class Stage:
                 elif dice == 4: tile = Diamond(game, j, i)
                 self.tiles[i].append(tile)
         self.tiles[5][5] = Miner(game, 5, 5)
+    
+    def within_bounds(self, x: int ,y: int ) -> bool:
+        return x >= 0 and y >= 0 and x < STAGE_WIDTH and y < STAGE_HEIGHT
 
-    def at(self, x, y) -> "Element":
-        if x < 0 or y < 0 or x >= STAGE_WIDTH or y >= STAGE_HEIGHT: return None
-        return self.tiles[y][x]
+    def at(self, x: int , y: int ) -> Optional["Element"]:
+        return self.tiles[y][x] if self.within_bounds(x,y) else None
 
-    def can_move(self, element, x, y) -> bool:
-        if x < 0 or y < 0 or x >= STAGE_WIDTH or y >= STAGE_HEIGHT: return False
+    def replace(self, e1 : "Element", e2 : "Element") -> None:
+        self.tiles[e1.y][e1.x] = e2
+
+    def can_move(self, element: "Element", x: int , y: int ) -> bool:
+        if not self.within_bounds(x,y): return False
         tile = self.at(x,y)
-        return tile is None or tile.can_enter(element)
+        return tile is None or tile.can_be_penetrated(element)
 
-    def try_move(self, element, x, y) -> bool:
+    def try_move(self, element: "Element", x: int , y: int ) -> bool:
         if not self.can_move(element, x, y): return False
         tile = self.at(x,y)
         self.tiles[element.y][element.x] = None
         self.tiles[y][x] = element
         element.x = x ; element.y = y
-        if not tile is None: element.has_entered(tile)
+        element.on_moved(tile)
         return True
 
-    def draw(self):
+    def draw(self) -> None:
         for row in self.tiles:
             for tile in row:
                 if not tile is None: tile.draw()
 
-    def on_update(self, delta_time):
+    def on_update(self, delta_time) -> None:
         for row in self.tiles:
             for tile in row:
                 if not tile is None: tile.on_update(delta_time)
@@ -59,12 +65,12 @@ class Game(arcade.Window):
         self.camera = None
         self.stage = None
 
-    def setup(self):
+    def setup(self) -> None:
         self.stage = Stage(self, 1)
         arcade.set_background_color(arcade.color.BLACK);
         self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-    def on_draw(self):
+    def on_draw(self) -> None:
         self.camera.use()
         self.clear()
         self.stage.draw()
@@ -74,7 +80,7 @@ class Game(arcade.Window):
     def on_update(self, delta_time):
         self.stage.on_update(delta_time)
 
-def main():
+def main() -> None:
     Game().setup()
     arcade.run()
 
--- elements.py
+++ elements.py
@@ -1,5 +1,6 @@
 import arcade
 import random
+from typing import Optional
 from boulder_dash import *
 
 TILE_SIZE = 64
@@ -7,85 +8,97 @@ TILE_SCALE = 0.75
 MAX_SPEED = 8 # squares per second
 
 class Element(arcade.Sprite):
-    def __init__(self, game, x, y):
+    def __init__(self, game: "Game", x: int, y: int) -> None:
         super().__init__("Tiles/" + type(self).__name__ + str(TILE_SIZE) + "-0.png", TILE_SCALE)
         self.game = game ;
         self.x = x ; self.y = y;
         self.center_x = TILE_SIZE * TILE_SCALE * (x + 0.5)
         self.center_y = TILE_SIZE * TILE_SCALE * (y + 0.5)
-        self.wait = 0
+        self.wait = 0 ; self.moved = self.moving = False
 
-    def try_move(self, ix, iy):
+    def try_move(self, ix: int, iy: int)  -> bool:
       if self.game.stage.try_move(self, self.x + ix, self.y + iy):
           self.center_x += TILE_SIZE * TILE_SCALE * ix
           self.center_y += TILE_SIZE * TILE_SCALE * iy
           self.wait = 1 / MAX_SPEED
+          self.moved = True
+          return True
+      return False
 
-    def on_update(self, delta_time):
+    def on_update(self, delta_time) -> None:
         if self.wait > 0: self.wait -= delta_time
-        else: self.tick()
+        else: 
+            self.moved = False
+            self.tick() 
+            self.moving = self.moved
 
     def tick(self):
         pass
 
-    def can_enter(self, entering):
+    def can_be_penetrated(self, by: "Element") -> bool:
         return False
 
-    def has_entered(self, into):
+    def on_moved(self, into: Optional["Element"]) -> None:
         pass
 
 class Soil(Element):
-    def __init__(self, game, x, y):
+    def __init__(self, game: "Game", x: int, y: int) -> None:
         super().__init__(game, x, y)
 
-    def can_enter(self, entering):
-        return isinstance(entering, Miner)
+    def can_be_penetrated(self, by) -> bool:
+        return isinstance(by, Miner)
 
 class Wall(Element):
-    def __init__(self, game, x, y):
+    def __init__(self, game: "Game", x: int, y: int) -> None:
         super().__init__(game, x, y)
 
-class Boulder(Element):
-    def __init__(self, game, x, y):
+class Rolling(Element):
+    def __init__(self, game: "Game", x: int, y: int) -> None:
         super().__init__(game, x, y)
 
-    def tick(self):
+    def tick(self) -> None:
         if self.try_move(0, -1): return
         ix = random.randint(0, 1) * 2 - 1;  # pick a side at random
-        if not self.try_roll(ix): self.try_roll(-ix)
+        self.try_roll(ix) or self.try_roll(-ix)
 
-    def try_roll(self, ix):
+    def try_roll(self, ix: int) -> bool:
         below = self.game.stage.at(self.x, self.y -1)
-        if not isinstance(below, Boulder): return False
+        if not isinstance(below, Rolling): return False
         if not self.game.stage.at(self.x + ix, self.y) is None : return False
         return self.try_move(ix, -1);
 
-    def has_entered(self, into):
+class Boulder(Rolling):
+    def __init__(self, game: "Game", x: int, y: int) -> None:
+        super().__init__(game, x, y)
+        
+    def on_moved(self, into: Optional["Element"]) -> None:
         if isinstance(into, Miner):
             pass # TODO : explode, game over
 
-class Diamond(Element):
-    def __init__(self, game, x, y):
+class Diamond(Rolling):
+    def __init__(self, game: "Game", x: int, y: int) -> None:
         super().__init__(game, x, y)
 
-    def can_enter(self, entering):
-        return isinstance(entering, Miner)
+    def can_be_penetrated(self, by: "Element") -> bool:
+        return isinstance(by, Miner)
 
-    def tick(self):
-        self.try_move(0, -1)
+    def on_moved(self, into: Optional["Element"]) -> None:
+        if isinstance(into, Miner): 
+            self.game.stage.replace(self, into)
+            into.score += 1
 
 class Miner(Element):
-    def __init__(self, game, x, y):
+    def __init__(self, game: "Game", x: int, y: int) -> None:
         super().__init__(game, x, y)
         self.score = 0
 
-    #def can_enter(self, entering):
-        #return isinstance(entering, Boulder) # TODO : NOT GOOD
+    def can_be_penetrated(self, by: "Element") -> bool:
+        return isinstance(by, Rolling) and by.moving
 
-    def has_entered(self, into):
+    def on_moved(self, into: Optional["Element"]) -> None:
         if isinstance(into, Diamond): self.score += 1
 
-    def tick(self):
+    def tick(self) -> None:
         if   arcade.key.LEFT  in self.game.keys:  
             if not self.try_move(-1, 0): self.try_push(-1)
         elif arcade.key.RIGHT in self.game.keys:  
@@ -95,5 +108,5 @@ class Miner(Element):
         elif arcade.key.DOWN  in self.game.keys:
             self.try_move(0, -1)
     
-    def try_push(self, ix):
+    def try_push(self, ix: int) -> bool:
         return False # TODO
