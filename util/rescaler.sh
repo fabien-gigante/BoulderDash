d3ef4b3233f11f4c3563792bc2a5a91b2816b7af
fabien-gigante <fabien.gigante@free.fr>
2022-08-14 11:10:05 +0200

    rescaler parallel

--- util/rescaler.sh
+++ util/rescaler.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 for img in ../res/*16-*.png; do
 	echo $img "->" ${img/16/64}
-	./ScalerTest_Windows -4xbrz  $img  ${img/16/64}
+	./ScalerTest_Windows -4xbrz  $img  ${img/16/64} &
 done
+wait
 #read -p "Press any key..."
